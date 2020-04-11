84de229ca30d8c56e83a17452138fc59d4e283d4
Ryan Compton <ryan.wade.compton@gmail.com>
2020-04-10 18:01:38 -0600

    complete view.js in routes.

--- routes/view.js
+++ routes/view.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 const path = require("path");
 
 //directly linking to html & set url
-
 router.get("/excercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"))
 });
 
 //one for stats too
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname,"../public/stats.html"))
+});
 
 module.exports = router;
