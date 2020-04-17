dbaeca2997c55b0935477c1b2776122348f2c6bb
Ryan Compton <ryan.wade.compton@gmail.com>
2020-04-17 15:59:54 -0600

    revert server file

--- server.js
+++ server.js
@@ -3,16 +3,30 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const PORT = 3000;
 const app = express();
+//if deployed, use the deployed database. Otherwise..
+var MONGODB_URI = process.env.MONGODB_URI || "monogodb://localhost/workout";
+
+const options = {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useFindAndModify: false, 
+  };
+
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// PRE_db_DEPLOY
+// mongoose.connect("mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+//db_DEPLOY
+mongoose.connect(MONGODB_URI, options);
 
 // routes
 app.use(require("./routes/api.js"));
