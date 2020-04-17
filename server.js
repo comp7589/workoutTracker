42353f49ef9c9b2dcb1b34f8e3efa29b0a4309ec
Ryan Compton <ryan.wade.compton@gmail.com>
2020-04-16 22:38:55 -0600

    reconfigure connection server.js.

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const PORT = 3000;
 const app = express();
-
-//if deployed, use the deployed database. Otherwise use mongoHeadlines db.
+//if deployed, use the deployed database. Otherwise..
 var MONGODB_URI = process.env.MONGODB_URI || "monogodb://localhost/workout";
 
+const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4 // Use IPv4, skip trying IPv6
+  };
 
 
 app.use(logger("dev"));
@@ -15,7 +20,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI);
+// PRE_db_DEPLOY
+// mongoose.connect("mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+//db_DEPLOY
+mongoose.connect(MONGODB_URI,options);
+
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
