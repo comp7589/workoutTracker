20326c60d7e4e64f2ee10bced236681b54a1f7dc
Ryan Compton <ryan.wade.compton@gmail.com>
2020-04-17 15:39:12 -0600

    save fix

--- routes/view.js
+++ routes/view.js
@@ -3,9 +3,9 @@ const path = require("path");
 
 //directly linking to html & set url
 
-router.get("/", (req, res) => {
-    res.redirect("/exercise");
-});
+// router.get("/", (req, res) => {
+//     res.redirect("/exercise");
+// });
 //??
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"))
